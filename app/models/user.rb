403bc8ac25f31c40caae2d2ec857fe192307f63f
wilsontheory <wilson.brian90@gmail.com>
2017-08-22 08:15:29 -0400

    Setup node package manager and webpack, setup frontend directories, successfully render a hello world div on server

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}

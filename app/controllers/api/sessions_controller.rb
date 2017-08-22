2e17d278f692bcc57c09f2f7670e717307f457ca
wilsontheory <wilson.brian90@gmail.com>
2017-08-21 23:17:09 -0400

    Add static page controller to handle first route, app works upon server boot

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username],
+    params[:user][:password])
+    if @user
+      render json: {}
+    else
+      render json: @user.errors.full_messages
+    end
   end
 
   def destroy
+    logout
+    render json: {}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace: api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     resources :user, only: [:create]
     resource :sessions, only: [:create, :destroy]
   end

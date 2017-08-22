6291dfcfea03ab0da5ae54d36ce56620110bd03b
wilsontheory <wilson.brian90@gmail.com>
2017-08-21 21:36:10 -0400

    Create routes for current resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace: api, defaults: { format: :json } do
+    resources :user, only: [:create]
+    resource :sessions, only: [:create, :destroy]
+  end
 end

42b2e5cfe1157b5c5752fdac51cc298c843f7e87
wilsontheory <wilson.brian90@gmail.com>
2017-08-21 22:45:41 -0400

    Make user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,18 @@
 class Api::UsersController < ApplicationRecord
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(user)
+      render json: {}
+    else
+      render json: @user.errors.full_messages
+    end 
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end

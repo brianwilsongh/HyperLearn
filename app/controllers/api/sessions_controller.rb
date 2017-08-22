564067730151d1099642e24e349ac773b235e85b
wilsontheory <wilson.brian90@gmail.com>
2017-08-22 18:13:35 -0400

    Fixed bug in session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,6 +3,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:user][:username],
     params[:user][:password])
     if @user
+      login(@user)
       render 'api/users/show'
     else
       render json: @user.errors.full_messages

de96fde6b5e7aaf4177adc38ada2cc2337c30ab7
wilsontheory <wilson.brian90@gmail.com>
2017-09-04 21:40:51 -0400

    Implement uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
-const path = require("path");
+// webpack.config.js
+var path = require("path");
+var webpack = require("webpack");
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+// include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/hyperlearn.jsx",
+  entry: "./frontend/<name of entry file>",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  devtool: 'source-maps',
+  plugins: plugins,
   module: {
     loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
-    },
-    {
-      test: /\.node$/,
-      loader: "node-loader"
-    }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
   }
 };

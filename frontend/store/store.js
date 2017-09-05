a0b01008215e6f1141f4cf83c5512ddecd6cd41a
wilsontheory <wilson.brian90@gmail.com>
2017-09-04 23:15:43 -0400

    Setup for heroku deployment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
 
 const generateStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 };
 
 export default generateStore;
